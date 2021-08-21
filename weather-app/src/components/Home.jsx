f5a3eb51c8b4942e73fe2114e705dbee243aa7d7
toddpeltzer <toddpeltzer14@gmail.com>
2021-08-21 14:04:43 -0700

    updated home page

--- weather-app/src/components/Home.jsx
+++ weather-app/src/components/Home.jsx
@@ -5,6 +5,10 @@ import Chicago from '../home-display/Chicago';
 import Houston from '../home-display/Houston'
 import Seattle from '../home-display/Seattle';
 import Miami from '../home-display/Miami'
+import Philadelphia from '../home-display/Philadelphia'
+import Phoenix from '../home-display/Phoenix'
+import SanJose from '../home-display/SanJose'
+import SanDiego from '../home-display/SanDiego'
 import '../styling/home.css'
 
 function Home(props) {
@@ -13,11 +17,15 @@ function Home(props) {
             <div>
                 <span className="popularCitiesTitle">Popular Cities</span>
                 <section className="homeContainer">
-                    <div className="homeCity"><NewYork /></div>
+                    <div className="homeCity"><Seattle /></div>
+                    <div className="homeCity"><SanJose /></div>
                     <div className="homeCity"><LosAngeles /></div>
+                    <div className="homeCity"><SanDiego /></div>
+                    <div className="homeCity"><Phoenix /></div>
                     <div className="homeCity"><Chicago /></div>
                     <div className="homeCity"><Houston /></div>
-                    <div className="homeCity"><Seattle /></div>
+                    <div className="homeCity"><Philadelphia /></div>
+                    <div className="homeCity"><NewYork /></div>
                     <div className="homeCity"><Miami /></div>
                 </section>
             </div>
